c6d73544329046c2fab3480cea2550a14a33a132
Nicole L <158861083+Nicole-CY@users.noreply.github.com>
2024-09-26 20:19:07 +1000

    feat: add feat-002

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const express = require("express");
 
 const app = express();
 
-app.length("/", (req, res) => {
+app.get("/", (req, res) => {
   res.send("Hello World");
+});
+
+app.get("/about", (req, res) => {
+  res.send("About Us");
 });
 
 app.listen(8080, () => {
